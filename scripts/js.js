1ee93efa044f2ae2b6ed06411a3ed40ffd01d909
oxana1510 <oksi666@xaker.ru>
2019-01-22 22:15:15 +0300

    .

--- scripts/js.js
+++ scripts/js.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
   fade: true,
   cssEase: false,
   centerMode: true,
+  adaptiveHeight: true,
   prevArrow: '<button type="button" class="slick-prev">Next</button>',
   nextArrow: '<button type="button" class="slick-next">Next</button>'
   });
